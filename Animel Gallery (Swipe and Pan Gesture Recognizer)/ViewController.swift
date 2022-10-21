260f5f3e1a0bc3fdb3c9773ff0fe5e2848eb7b43
Junaed <junaed1995@gmail.com>
2022-10-21 11:29:04 +0600

    Initial Commit

---
+++ Animel Gallery (Swipe and Pan Gesture Recognizer)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animel Gallery (Swipe and Pan Gesture Recognizer)
+//
+//  Created by Junaed Muhammad Chowdhury on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
